8e32517f73c89ccc6b80d42513e49dcc01114e7f
Marchal yoan <marchalyoan@gmail.com>
2026-05-09 21:33:37 +0200

    build: bump versionCode to 5 (versionCode 4 already used on Play Store)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.ym.carbucheap"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
